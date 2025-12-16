21b172ffda9fac0a4b07c81e724b4db78eec21b6
Unus Gaffoor <unus@uniqconsulting.com.au>
2025-12-16 18:23:31 +1100

    Move auto complete key to space attribute

--- packages/gbmembers/src/components/Member/MemberEdit.js
+++ packages/gbmembers/src/components/Member/MemberEdit.js
@@ -1084,7 +1084,10 @@ export const MemberEdit = ({
               <span className="line">
                 <Autocomplete
                   id="addressAutoComplete"
-                  apiKey={'AIzaSyA-tujnpf8Jy33hVaJ_9GtRdMgHw4jvnwo'}
+                  apiKey={getAttributeValue(
+                    this.props.space,
+                    'AutoCompleteKey',
+                  )}
                   placeholder="Lookup Address"
                   style={{
                     width: '400px',
